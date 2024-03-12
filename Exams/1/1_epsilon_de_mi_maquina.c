4e2bce19de3f764e1a74600a5028fdb43531db85
andromedalactea <bry3639@gmail.com>
2024-03-12 11:37:13 -0500

    [FEAT] strat exam 1

---
+++ Exams/1/1_epsilon_de_mi_maquina.c
@@ -0,0 +1,36 @@
+/*
+El algoritmo comienza con un valor de epsilon igual a 1.0. Luego,
+entra en un bucle que reduce epsilon a la mitad en cada iteración.
+En cada paso, el algoritmo verifica si el valor actual de epsilon,
+cuando se suma a 1.0, resulta en un valor diferente a 1.0.
+Si al sumar la mitad del epsilon actual a 1.0 el resultado sigue siendo 1.0,
+significa que epsilon aún no es lo suficientemente pequeño como para ser
+considerado el épsilon de la máquina. El bucle continúa hasta que se encuentra
+el valor más pequeño de epsilon que, al sumarse a 1.0, produce un valor detectable
+como diferente de 1.0.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Inicializamos epsilon con un valor de 1.0. 
+    // Este será nuestro punto de partida para encontrar el menor valor
+    // que, al sumarse a 1.0, el resultado se diferencia de 1.0.
+    double epsilon = 1.0;
+
+    // El bucle continúa hasta que 1.0 + epsilon/2 sea igual a 1.0,
+    // lo que significa que hemos encontrado el menor incremento 
+    // que no produce cambio al sumarse a 1.0.
+    // En cada iteración, dividimos epsilon por 2 para hacerlo más pequeño,
+    // acercándonos gradualmente al épsilon de la máquina.
+    while ((double)(1.0 + (epsilon / 2.0)) != 1.0) {
+        epsilon /= 2.0;
+    }
+
+    // Una vez encontrado, imprimimos el valor de epsilon,
+    // que representa el épsilon de la máquina para el tipo de datos double.
+    // Este valor es el menor número que, sumado a 1.0, el resultado
+    // es detectable como diferente de 1.0 por la computadora.
+    printf("Épsilon de la máquina (double): %g\n", epsilon);
+    return 0;
+}
